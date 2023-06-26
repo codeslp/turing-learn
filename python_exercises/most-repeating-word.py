91209ab8c495d10083d6848e5ab88faccc2946a3
Brian Farish <brian.farish@gmail.com>
2023-06-26 14:30:40 -0500

    added optimal example

--- python_exercises/most-repeating-word.py
+++ python_exercises/most-repeating-word.py
@@ -8,3 +8,17 @@ for word in words[1:]:
     if c[0][1] > Counter(repeater).most_common(1)[0][1]:
         repeater = word
 print(repeater)
+
+
+# badass optimal example: 
+from collections import Counter
+
+WORDS = ['this', 'is', 'an', 'elementary', 'test', 'example']
+
+def most_repeating_letter_count(word):                        
+    return Counter(word).most_common(1)[0][1]                 
+
+def most_repeating_word(words):
+    return max(words, key=most_repeating_letter_count)
+
+print(most_repeating_word(WORDS))
